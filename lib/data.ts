b72d10c3432d3a9f180eebd4a83ffdd486b83b33
unknown <refurbishing3@gmail.com>
2025-05-18 00:20:47 +0630

    init

--- lib/data.ts
+++ lib/data.ts
@@ -144,7 +144,7 @@ export const universities: University[] = [
       "The Massachusetts Institute of Technology (MIT) is a private research university in Cambridge, Massachusetts. Founded in 1861, MIT has since played a key role in the development of modern technology and science, ranking among the top academic institutions in the world.",
     website: "mit.edu",
     address: "77 Massachusetts Ave, Cambridge, MA 02139, United States",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/mit-university-logo-vector-01-A82BjBJwg6wWTRmhpQ1D8Ms0mA5U9M.png",
+    logo: "/mit.png",
     banner:
       "https://alum.mit.edu/sites/default/files/images/MIT_77MassAve_EmilyDahl.jpg",
     color: "#A31F34",
@@ -223,7 +223,7 @@ export const universities: University[] = [
       "Stanford University, officially Leland Stanford Junior University, is a private research university in Stanford, California. The university was founded in 1885 by Leland and Jane Stanford in memory of their only child, Leland Stanford Jr., who had died of typhoid fever at age 15 the previous year.",
     website: "stanford.edu",
     address: "450 Serra Mall, Stanford, CA 94305, United States",
-    logo: "https://identity.stanford.edu/wp-content/uploads/sites/3/2020/07/block-s-right.png",
+    logo: "/stanford.png",
     banner: "https://visit.stanford.edu/assets/cardinal/images/explore-campus/campus-planning.jpg",
     color: "#8C1515",
     majors: [
@@ -301,7 +301,7 @@ export const universities: University[] = [
       "The University of Cambridge is a collegiate research university in Cambridge, United Kingdom. Founded in 1209, the University of Cambridge is the second-oldest university in the English-speaking world and the world's fourth-oldest surviving university. It grew out of an association of scholars who left the University of Oxford after a dispute with the townspeople.",
     website: "cam.ac.uk",
     address: "The Old Schools, Trinity Ln, Cambridge CB2 1TN, United Kingdom",
-    logo: "https://static.cdnlogo.com/logos/u/49/university-of-cambridge.png",
+    logo: "/cambridge.png",
     banner:
       "https://www.visitcambridge.org/app/uploads/2021/11/corpus-christi-1600x0-c-default.jpg",
     color: "#A3C1AD",
@@ -380,7 +380,7 @@ export const universities: University[] = [
       "The University of Oxford is a collegiate research university in Oxford, England. There is evidence of teaching as early as 1096, making it the oldest university in the English-speaking world and the world's second-oldest university in continuous operation. The university is made up of 39 constituent colleges, and a range of academic departments.",
     website: "ox.ac.uk",
     address: "Wellington Square, Oxford OX1 2JD, United Kingdom",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/294a76cd1bd62b3786cdd4e79344f100-bkRfsX6t0omMg6cpbXVtF4BfKcdWlw.png",
+    logo: "/oxford.png",
     banner:
       "https://cdn.getyourguide.com/img/location/5c10eb21206c9.jpeg/99.jpg",
     color: "#002147",
@@ -459,7 +459,7 @@ export const universities: University[] = [
       "The California Institute of Technology (Caltech) is a private research university in Pasadena, California. The university is known for its strength in science and engineering, and is one among a small group of institutes of technology in the United States which is primarily devoted to the instruction of pure and applied sciences.",
     website: "caltech.edu",
     address: "1200 E California Blvd, Pasadena, CA 91125, United States",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/27d99d317a36dee6a2fc82b102d090cc-4DENeX6bfsHIXappQvbXAY5XOG3oor.png",
+    logo: "/caltech.png",
     banner:
       "https://media.nbclosangeles.com/2019/09/GettyImages-488587347.jpg?quality=85&strip=all&resize=1200%2C675",
     color: "#FF6C0C",
@@ -538,7 +538,7 @@ export const universities: University[] = [
       "ETH Zurich (Swiss Federal Institute of Technology in Zurich) is a public research university in the city of Zürich, Switzerland. Founded by the Swiss Federal Government in 1854 with the stated mission to educate engineers and scientists, the school focuses primarily on science, technology, engineering, and mathematics.",
     website: "ethz.ch",
     address: "Rämistrasse 101, 8092 Zürich, Switzerland",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image.imageformat.lightbox-lBhL7eJKEiVYymadfenwxJeWiVhFh0.png",
+    logo: "/eth-zurich.png",
     banner:
       "https://ethz.ch/en/campus/access/zentrum/_jcr_content/par/fullwidthimage/image.imageformat.1286.152068286.jpg",
     color: "#1D4289",
@@ -617,7 +617,7 @@ export const universities: University[] = [
       "The University of Chicago is a private research university in Chicago, Illinois. Founded in 1890, the university is known for its rigorous academic and intellectual traditions. The university consists of the College, various graduate programs, and interdisciplinary committees organized into five academic research divisions.",
     website: "uchicago.edu",
     address: "5801 S Ellis Ave, Chicago, IL 60637, United States",
-    logo: "https://i0.wp.com/biocars.uchicago.edu/wp-content/uploads/2019/05/logo.png?fit=1200%2C1200&ssl=1&w=640",
+    logo: "/chicago.png",
     banner: "https://news.uchicago.edu/sites/default/files/styles/full_width/public/images/image/20180315/20171016campusfall1761_0.jpg?itok=hoPJRrK_",
     color: "#800000",
     majors: [
@@ -695,7 +695,7 @@ export const universities: University[] = [
       "Imperial College London is a public research university in London, United Kingdom. Its history began with Prince Albert, consort of Queen Victoria, who developed his vision for a cultural area that included the Royal Albert Hall, Victoria & Albert Museum, Natural History Museum and royal colleges.",
     website: "imperial.ac.uk",
     address: "South Kensington Campus, London SW7 2AZ, United Kingdom",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imperial-college-london-logo-png_seeklogo-381211-z60bph18NWdLl4lLg0sTGBOzhyC4fR.png",
+    logo: "/imperial.png",
     banner:
       "https://www.imperial.ac.uk/stories/south-kensington-campus-evolution/assets/2fDjW3VjdA/main-aerial-shot-wide-2560x1440.jpeg",
     color: "#003E74",
@@ -774,7 +774,7 @@ export const universities: University[] = [
       "The University of Tokyo, abbreviated as Todai, is a public research university located in Tokyo, Japan. Established in 1877, it is the first of Japan's National Seven Universities. The university has 10 faculties, 15 graduate schools and enrolls about 30,000 students, 2,100 of whom are international students.",
     website: "u-tokyo.ac.jp",
     address: "7 Chome-3-1 Hongo, Bunkyo City, Tokyo 113-8654, Japan",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/png-transparent-university-of-tokyo-kavli-institute-for-the-physics-and-mathematics-of-the-universe-research-university-tokyo-leaf-symmetry-university-thumbnail-k9YPiW48RO9LlHqpur3Bv3RYst1GpJ.png",
+    logo: "/tokyo.png",
     banner: "https://www.u-tokyo.ac.jp/content/400027399.jpg",
     color: "#0C2D83",
     majors: [
@@ -852,7 +852,7 @@ export const universities: University[] = [
       "The National University of Singapore (NUS) is the first autonomous research university in Singapore. Founded in 1905, it is the oldest higher education institution in Singapore. NUS is a comprehensive research university, offering a wide range of disciplines, including the sciences, medicine and dentistry, design and environment, law, arts and social sciences, engineering, business, computing and music.",
     website: "nus.edu.sg",
     address: "21 Lower Kent Ridge Rd, Singapore 119077",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/firefox_ASBPX4EeXC-removebg-preview-8wUH802hoASgdnonKb9lFUVxi4oZ1D.png",
+    logo: "/nus.png",
     banner: "https://gov-web.s3.ap-northeast-1.amazonaws.com/uploads/2018/04/NUS-ERC.jpg",
     color: "#003D7C",
     majors: [
@@ -930,7 +930,7 @@ export const universities: University[] = [
       "Peking University is a major research university in Beijing, China, and a member of the elite C9 League of Chinese universities. Founded as the Imperial University of Peking in 1898, it is the first modern national university established in China. It also served as the highest administration for education in China at the beginning of its founding.",
     website: "pku.edu.cn",
     address: "5 Yiheyuan Rd, Haidian District, Beijing, China, 100871",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Peking_University_seal.svg-mR9vAdzpaTvk9yMuTh8S1nkzXSv0kK.png",
+    logo: "/peking.png",
     banner: "https://english.pku.edu.cn/Uploads/Bden/File/2020/11/09/i3300z10re5.png",
     color: "#91191C",
     majors: [
@@ -1008,7 +1008,7 @@ export const universities: University[] = [
       "The University of Melbourne is a public research university located in Melbourne, Australia. Founded in 1853, it is Australia's second oldest university and the oldest in Victoria. Melbourne's main campus is located in Parkville, an inner suburb north of the Melbourne central business district.",
     website: "unimelb.edu.au",
     address: "Parkville VIC 3010, Australia",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/366298928_690067039828240_7479680550951410573_n.jpg-Z59UAHJUGKBWb3WrJJgUnPHkXggLhP.jpeg",
+    logo: "/melbourne.png",
     banner: "https://www.unimelb.edu.au/__data/assets/image/0012/3798804/video.jpg",
     color: "#0C2340",
     majors: [
@@ -1086,7 +1086,7 @@ export const universities: University[] = [
       "The University of Toronto is a public research university in Toronto, Ontario, Canada, located on the grounds that surround Queen's Park. It was founded by royal charter in 1827 as King's College, the first institution of higher learning in the colony of Upper Canada.",
     website: "utoronto.ca",
     address: "27 King's College Cir, Toronto, ON M5S 1A1, Canada",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/university-of-toronto-logo-svg-vector-0Otdn4Ro5PfOBJjc9VvImIq0qrY81z.svg",
+    logo: "/toronto.svg",
     banner: "https://media.istockphoto.com/id/94506078/photo/university-of-toronto.jpg?s=612x612&w=0&k=20&c=LczMUDuYYMCwj_SOFK8iOnzcGAE9RT_RJcZ3rvhg6_s=",
     color: "#002A5C",
     majors: [
@@ -1164,7 +1164,7 @@ export const universities: University[] = [
       "Tsinghua University is a major research university in Beijing, and a member of the elite C9 League of Chinese universities. Since its establishment in 1911, it has graduated numerous Chinese leaders in politics, business, academia, and culture.",
     website: "tsinghua.edu.cn",
     address: "30 Shuangqing Rd, Haidian District, Beijing, China, 100084",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Tsinghua_University_Logo.svg-OAi4VxB8M3BksFUR9myrIGgOCWMSC9.png",
+    logo: "/tsinghua.png",
     banner: "https://www.tsinghua.edu.cn/__local/1/F9/AB/76A4BA1F070CB4CEF8A6209841F_AE779678_DFFA3.jpg",
     color: "#660874",
     majors: [
@@ -1242,7 +1242,7 @@ export const universities: University[] = [
       "The University of California, Berkeley is a public research university in Berkeley, California. Established in 1868 as the state's first land-grant university, it is the oldest campus of the University of California system and a founding member of the Association of American Universities.",
     website: "berkeley.edu",
     address: "Berkeley, CA 94720, United States",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/smH1kaO7SvROZrkTdX9t69JMpQcaWufFdHvmddoegk6guEADpzl56_zV33jXrce9jnJepML46vikk1g0eufsv6kuh7RGYXS3ggyO4aGZqjDJ3wEKVorN6cWzjLWxkhZCDMz2d-f--EizbzWba03VCg-aD9m9ZsIvV34VVQiPs2fBvBrbZjk81.svg",
+    logo: "/berkeley.svg",
     banner:
       "https://www.tclf.org/sites/default/files/thumbnails/image/CA_Berkeley_UniversityOfCaliforniaAtBerkeley_byCharlieNguyen-Flickr_2008_001_Sig.jpg",
     color: "#003262",
@@ -1321,7 +1321,7 @@ export const universities: University[] = [
       "The University of Sydney is an Australian public research university in Sydney, Australia. Founded in 1850, it is Australia's first university and is regarded as one of the world's leading universities. The university is known as one of Australia's 6 sandstone universities.",
     website: "sydney.edu.au",
     address: "Camperdown NSW 2006, Australia",
-    logo: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/University_of_Sydney_coat_of_arms.png/250px-University_of_Sydney_coat_of_arms.png",
+    logo: "/sydney.png",
     banner: "https://media.istockphoto.com/id/1486072206/photo/the-facade-of-the-historical-university-of-sydney-quadrangle-in-cloudy-days.jpg?s=612x612&w=0&k=20&c=mn8vpnNrepDqzzdAbh3Rg993Z0JiKDEmrsVN2dzxP3I=",
     color: "#E64626",
     majors: [
@@ -1399,7 +1399,7 @@ export const universities: University[] = [
       "The University of Cape Town is a public research university located in Cape Town in the Western Cape province of South Africa. UCT was founded in 1829 as the South African College, and is the oldest higher education institute in South Africa.",
     website: "uct.ac.za",
     address: "Rondebosch, Cape Town, 7700, South Africa",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/University_of_Cape_Town_logo.svg-zu1FswFfJpFK2S5RTAwPjhJekeAwxa.png",
+    logo: "/cape-town.png",
     banner:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/UCT_Upper_Campus_landscape_view.jpg/960px-UCT_Upper_Campus_landscape_view.jpg",
     color: "#005883",
@@ -1478,7 +1478,7 @@ export const universities: University[] = [
       "The University of São Paulo is a public university in the Brazilian state of São Paulo. It is the largest Brazilian public university and the country's most prestigious educational institution, the best university in Ibero-America, and holds a high reputation among world universities.",
     website: "usp.br",
     address: "R. da Reitoria, 374 - Cidade Universitária, São Paulo - SP, 05508-220, Brazil",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Webysther_20170627_-_Bras%C3%A3o_USP.svg",
+    logo: "/sao-paulo.svg",
     banner: "https://cdn.britannica.com/96/141196-050-B8A6528A/Paulista-Museum-part-gardens-University-of-Sao.jpg",
     color: "#1C3A70",
     majors: [
@@ -1556,7 +1556,7 @@ export const universities: University[] = [
       "Seoul National University is a national research university located in Seoul, South Korea. It is one of the flagship Korean national universities and is considered to be the most prestigious university in the country. Founded in 1946, Seoul National University is the first national university in South Korea.",
     website: "snu.ac.kr",
     address: "1 Gwanak-ro, Gwanak-gu, Seoul, South Korea",
-    logo: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Seoul_national_university_emblem.svg-E3MO6rT8gjOSpVPA201x0Z2Q3JfTyI.png",
+    logo: "/seoul.png",
     banner: "https://manoa.hawaii.edu/mix/wp-content/uploads/2023/07/Gwanak-Campus-in-Autumn-scaled-2000x1000.jpg",
     color: "#00205B",
     majors: [
